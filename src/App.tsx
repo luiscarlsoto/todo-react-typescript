8dbae26c7e3e936d36a3c64051920c39c7960603
Luis <luiscarlsoto@gmail.com>
2023-03-06 03:23:09 -0400

    [UPDATE] Router

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { Navigate } from "react-router";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { getToken } from "./features/auth/authActions";
+import { Dashboard } from "./pages/dashboard/Dashboard";
+import { Login } from "./pages/login";
+import { useAppSelector } from "./store/hooks";
+
+interface IValidateRoute {
+    authRequired?: boolean;
+    to: string;
+    element: React.ElementType;
+}
+
+const ValidateRoute = ({
+    authRequired,
+    to,
+    element: Element,
+}: IValidateRoute) => {
+    const isLogged = useAppSelector(getToken);
+    console.log({ isLogged });
+    const location = useLocation();
+    console.log(Boolean(isLogged));
+    if (authRequired) {
+        if (!isLogged) {
+            return <Navigate to={to} state={{ from: { location } }} replace />;
+        }
+    } else {
+        if (isLogged) {
+            return <Navigate to={to} state={{ from: { location } }} replace />;
+        }
+    }
+    return <Element />;
+};
+
+const App = () => {
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <ValidateRoute
+                        to="/login"
+                        authRequired
+                        element={Dashboard}
+                    />
+                }
+            />
+            <Route
+                path="/login"
+                element={<ValidateRoute to="/" element={Login} />}
+            />
+            <Route
+                path="/register"
+                element={<ValidateRoute to="/" element={Login} />}
+            />
+        </Routes>
+    );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
