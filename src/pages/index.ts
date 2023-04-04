0a3450c4999f544630443c82e81e18857ddb3b72
Luis <luiscarlsoto@gmail.com>
2023-04-03 20:01:28 -0400

    [ADD] Pages index

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { ErrorPage } from './errorPage/ErrorPage';
+export { Dashboard } from './dashboard/Dashboard';
+export { Profile } from './profile/Profile';
+export { Login } from './login';
