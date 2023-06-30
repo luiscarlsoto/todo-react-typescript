907934452529baae5ffda50758d7bdc02018532d
Luis Soto <luiscarlsoto@gmail.com>
2023-06-30 10:05:01 -0400

    [UPDATE] Fetch props

--- src/api/fetchApi.ts
+++ src/api/fetchApi.ts
@@ -1,23 +1,30 @@
 import axios from 'axios';
 import { apiURL } from '../helper/constants.helper';
+import { token } from '../features/auth/authSlice';
 
 export interface TUseFetchParams {
-    path?: string;
+    path?: string | number;
     route: string;
-    method?: 'get' | 'post' | 'put' | 'delete';
+    method?: 'get' | 'post' | 'put' | 'delete' | 'patch';
     data?: object;
     params?: object;
+    withToken?: boolean;
 }
 
 export const fetchApi = async (props : TUseFetchParams) => {
-    const { route, method, path, data, params } = props;
+    const { route, method, path, data, params, withToken = true } = props;
+
     const routeAux = path ? `${route}/${path}` : route;
 
+    const headers = {
+        Authorization:  withToken ? `Bearer ${token}` : ''
+    }
     const response = await axios({
         method: method || 'get',
         url: `${apiURL}/${routeAux}`,
         data,
         params,
+        headers
     })
     return response;
 
